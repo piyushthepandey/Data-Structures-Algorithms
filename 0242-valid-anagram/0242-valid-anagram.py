aeac9e0716a48c0b9d37ea3c1dad1d9f0915915f
Piyush <145172249+piyushthepandey@users.noreply.github.com>
2024-06-05 01:23:15 -0400

    Time: 64 ms (5.81%), Space: 17.4 MB (24.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,35 +1,8 @@
-class Solution(object):
-    def isAnagram(self, s, t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a = sorted(s)
+        b = sorted(t) 
 
-
-        #two strings s and t 
-        #return True 
-        #If t is an anagram of s else false 
-
-
-        #create a hashmap 
-        #key s, t and value will frequency of occurence 
-
-        #if value of s, t % 2 return true else false 
-        
-        if len(s)==len(t):
-            my_dict = {} 
-            for i in s: 
-                print(i)
-                if i in my_dict:
-                    my_dict[i]+=1
-                else:
-                    my_dict[i]=1
-            for j in t: 
-                if j in my_dict: 
-                    my_dict[j] -= 1 
-            for k in my_dict.values(): 
-                if k != 0:
-                    return False
+        if(a==b): 
             return True
-        else:
-            return False
-        
-
-               
-
+        return False
