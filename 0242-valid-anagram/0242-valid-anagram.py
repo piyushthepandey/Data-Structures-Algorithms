4944c1877d7a69fefedebb9f4b7d070550a5905f
Piyush <145172249+piyushthepandey@users.noreply.github.com>
2024-06-05 01:44:28 -0400

    Time: 44 ms (80.23%), Space: 16.9 MB (87.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        a = sorted(s)
-        b = sorted(t) 
+        my_dict={}
+        for i in s:
+            if i not in my_dict:
+                my_dict[i]=1
+            else:
+                my_dict[i]+=1
 
-        if(a==b): 
-            return True
-        return False
+        for i in t:
+            if i not in my_dict:
+                my_dict[i]=1
+            else:
+                my_dict[i]-=1
+                
+        flag = all(value == 0 for value in my_dict.values())
+        return flag
